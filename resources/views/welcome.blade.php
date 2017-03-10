1f1216860c62fd7d0d2ed34c77764e3056acfe77
Shahzaad Ismail <sismail@theworkingearth.com>
2017-03-10 13:46:29 +0200

    form updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,56 +12,7 @@
 
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
+            
         </style>
     </head>
     <body>
@@ -79,20 +30,24 @@
 
             <div class="content">
                 
-              {!! Form::open(['url' => 'foo/bar']) !!}
+              {!! Form::open(['url' => 'Account/Login']) !!}
               
               
+              <div class="form-group"> 
+                  <span> Email:  </span> {!! Form::email('email') !!}
+              </div> 
+              <div class="form-group"> 
+                  <span> Password:  </span> {!! Form::password('password') !!}
+              </div> 
+              <div class="form-group">  
               {!! 
-              
-                    Form::email('email', null, null)
-                
-                !!}
-                {!! 
                     Form::submit('Sign In!')
               
-                !!}
+              !!}
+              </div>
+              
                 
-                <input type="button" onclick="window.location='https://php.net'" value="Sign Up"/>
+                <input type="button" onclick="window.open('https://php.net')" value="Sign Up"/>
               {!! Form::close() !!}
 
                
