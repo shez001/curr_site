3c6498060a7430186345c6e9bae4026e041bf4ff
Shahzaad Ismail <shez@live.co.za>
2017-03-12 23:17:36 +0200

    update database for currency table

---
+++ database/migrations/2017_03_12_090357_update_currency_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateCurrencyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('currencies', function (Blueprint $table) {
+            $table->string('code')->unique()->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('currencies', function (Blueprint $table) {
+            $table->dropColumn(['code']);
+        });
+    }
+}
